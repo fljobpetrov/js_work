a90a23b7468dfdcba82eb31ccad719dfc1684561
Stieglitz <fl_job_petrov@mail.ru>
2020-03-16 11:06:57 +0300

    First commit

---
+++ wk_1/script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+console.log(`hello ${"name"}`);
+console.log(typeof new Boolean(true));
+//  alert(admin);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false    
+}
+
+console.log(persone["name"]);
+console.log(persone.age);
+
+let arr = ['plum.png', 'orange.png', 'apple.png'];
+
+console.log(arr[2]);
+
+let x = +"w123z";
+console.log(typeof(x));
+console.log(x);
+
+
+
+let q;
+q = prompt('Какое «официальное» название JavaScript?', '');
+
+if (q == 'ECMAScript') {
+    alert('Yes');
+} else {
+    alert('Не знаете? ECMAScript!');
+}
