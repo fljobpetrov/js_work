4b0e777ca5122ffb926519992562c62cf5bd7a09
Stieglitz <fl_job_petrov@mail.ru>
2020-03-17 16:51:10 +0300

    ex_1

---
+++ ex_1/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let money = prompt('Бюджет в месяц');
+let time = new Date(prompt('Введите дату в формате YYYY-MM-DD'));
+
+let expenseItem1 = prompt('Введите обязательную статью расходов в этом месяце');
+let sum1 = prompt("Во сколько обойдется?");
+
+let expenseItem2 = prompt('Введите обязательную статью расходов в этом месяце');
+let sum2 = prompt("Во сколько обойдется?");
+
+let expenses = {
+    [expenseItem1]: sum1,
+    [expenseItem2]: sum2,
+};
+
+let optionalExpenses = {};
+
+let income = [];
+
+let appData = {
+    money: money,
+    timeData: time, 
+    expenses: expenses,
+    optionalExpenses: optionalExpenses,
+    income: income,
+    savings: false   
+};
+
+console.log(appData.money);
+console.log(appData.timeData);
+console.log(appData.expenses);
